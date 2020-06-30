b4c066a8b5ba498c4361f200a8a274d1b3045d29
Hritik Maheshwari <60053891+CRAZYGEEKS04@users.noreply.github.com>
2020-06-30 23:00:30 +0530

    Add files via upload

---
+++ MissingNumber.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int main()
+{
+    long int n,sum=0,sum1=0,arr[300000],opt;
+	cin>>n;
+	sum=(n*(n+1))/2;
+	for(int i=0;i<n-1;i++)
+	{
+		cin>>arr[i];
+	}
+	for(int j=0;j<n-1;j++)
+	{
+		sum1+=arr[j];
+	}
+	opt=sum-sum1;
+	cout<<opt;
+	return 0;
+}
